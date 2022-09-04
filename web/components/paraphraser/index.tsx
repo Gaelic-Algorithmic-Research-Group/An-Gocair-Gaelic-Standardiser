7dfaab0cb52f8a08a934c6c73f70c895ee9572d5
xihajun <jhuang10@ed.ac.uk>
2022-09-04 18:09:17 +0000

    feat: add new examples

--- web/components/paraphraser/index.tsx
+++ web/components/paraphraser/index.tsx
@@ -73,7 +73,14 @@ export default function Paraphraser() {
 
   const loadSameple = () => {
     var text = "here the text that you want to input.";
-    var array = ["Cha'n 'eil mi 'fuirach 'nam thigh mór an-seo ann an Éirinn, gu mi-fhortanach, ach siod a' cheud tigh a bh' agam-sa", "bhu ad a fuireach ann an Inhbir-Nìs an uar sen."]
+    var array = ["Cha'n 'eil mi 'fuirach 'nam thigh mór an-seo ann an Éirinn, gu mi-fhortanach, ach siod a' cheud tigh a bh' agam-sa",
+    	      	"bhu ad a fuireach ann an Inhbir-Nìs an uar sen.",
+		"Luchd-reic Charbad, Acuinn agus Thruncaichean.",
+		"Tha ar prisean iosal, agus tha 'm bathar dhe'n t-seorsa 's fhearr.",
+		"Thòisich leann-dubh air buaidh fhaotainn air Màiri bhochd.",
+		"TD MU DHAOINE GHEIBH BOGADH BATHAIDH, ATH-BHEOTHACHADH.",
+		"'S é 'n duine nuadh a theirear ris an nuadh chreatuir so,",
+		"Tha an cogadh a sior dhol air adhart ann an Cuba."]
     var randomtext = array[Math.floor(Math.random() * array.length)];
     //document.querySelector('textarea').value = randomtext;
     setInputText(randomtext);
