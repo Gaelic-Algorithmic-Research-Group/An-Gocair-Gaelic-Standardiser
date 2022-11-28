66d371c3ecf09737e5b5dca5e5317270c0d76496
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2022-11-28 17:00:52 +0000

    add pre-commit hook for black python formatter

--- cli/convert.py
+++ cli/convert.py
@@ -6,35 +6,37 @@ import argparse
 import os.path
 import sys
 
+
 def cli(args=None):
-    # If no arguments are specified, take the list of  
+    # If no arguments are specified, take the list of
     # command line arguments passed to the Python script
     if not args:
         args = sys.argv[1:]
     argparser = argparse.ArgumentParser()
-    argparser.add_argument('--text', type=str, help='text to translate')
-    argparser.add_argument('--file', type=str, help='file to translate')
+    argparser.add_argument("--text", type=str, help="text to translate")
+    argparser.add_argument("--file", type=str, help="file to translate")
     # The parse_args() function will use an argument list if specified,
     # otherwise it will use the passed command line arguments
     args = argparser.parse_args(args)
     convert(args.text, args.file)
+
 
 def convert(text, file):
     # Load the model
     # TODO this outputs a bunch of model info to the screen - can we avoid it?
     # TODO include flag in argparse for model
     dirname = os.path.dirname(__file__)
-    models_path = os.path.join(dirname, 'models/')
-    binary_path = os.path.join(dirname, 'binary/')
+    models_path = os.path.join(dirname, "models/")
+    binary_path = os.path.join(dirname, "binary/")
     pre2goc = TransformerModel.from_pretrained(
         models_path,
-        checkpoint_file='checkpoint_best.pt', # model loc
-        data_name_or_path=binary_path, # binary data path
+        checkpoint_file="checkpoint_best.pt",  # model loc
+        data_name_or_path=binary_path,  # binary data path
     )
 
     def translate(inputs):
-        inputs = " ".join(list(inputs.replace(" ","_")))
-        return pre2goc.translate(inputs).replace(" ","").replace("_"," ")
+        inputs = " ".join(list(inputs.replace(" ", "_")))
+        return pre2goc.translate(inputs).replace(" ", "").replace("_", " ")
 
     if text:
         # Check string is not empty
@@ -45,8 +47,8 @@ def convert(text, file):
 
     if file:
         if os.path.isfile(file):
-            with open(file, 'r') as f:
-                file_text = f.readlines() # returns every line in the file in a list
+            with open(file, "r") as f:
+                file_text = f.readlines()  # returns every line in the file in a list
         else:
             raise Exception("File does not exist")
 
@@ -54,32 +56,41 @@ def convert(text, file):
         text_test = " ".join(file_text)
         text_test = text_test.strip()
         if not text_test:
-            raise ValueError('File is empty')
+            raise ValueError("File is empty")
 
         # process the text sentence by sentence, respecting newline characters
-        translated_text = "" # initialize the translated text string
-        file_text = [line.strip() for line in file_text] # note that strip removes newline characters
-        is_text = [bool(line) for line in file_text] # empty strings in the list are False and they denote new paragraphs
+        translated_text = ""  # initialize the translated text string
+        file_text = [
+            line.strip() for line in file_text
+        ]  # note that strip removes newline characters
+        is_text = [
+            bool(line) for line in file_text
+        ]  # empty strings in the list are False and they denote new paragraphs
         for ii in range(len(file_text)):
             # loop through every line in the text, split the lines into sentences and translate sentence by sentence.
             # the end of a sentence is identified by a '.'
             if is_text[ii]:
                 line = file_text[ii]
-                for sentence in line.split('.'):
+                for sentence in line.split("."):
                     if sentence:
-                        sentence = sentence.strip() + '.'
-                        translated_sentence = translate(sentence) # Run the model
+                        sentence = sentence.strip() + "."
+                        translated_sentence = translate(sentence)  # Run the model
                         translated_text += translated_sentence + " "
-                translated_text += "\n" # at the end of the line, insert a new line character
+                translated_text += (
+                    "\n"  # at the end of the line, insert a new line character
+                )
             else:
-                translated_text += "\n" # start new paragraph
+                translated_text += "\n"  # start new paragraph
         # write the translated text to disk
         # TODO This is a bit of a mess
-        with open(os.path.join(os.path.dirname(file), "pred." + os.path.basename(file)), 'w') as f:
+        with open(
+            os.path.join(os.path.dirname(file), "pred." + os.path.basename(file)), "w"
+        ) as f:
             f.write(translated_text)
 
     if not text and not file:
         raise Exception("Please specify either the text or the file to translate")
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     cli()
