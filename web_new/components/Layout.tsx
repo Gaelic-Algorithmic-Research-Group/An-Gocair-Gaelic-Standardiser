702435045abe78596f869075323bd460f4b892d8
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-02 14:27:56 +0000

    fix layout following changes in React 18

--- web_new/components/Layout.tsx
+++ web_new/components/Layout.tsx
@@ -1,6 +1,12 @@
 import Footer from "@/components/Footer";
+import React from "react";
 
-const Layout: React.FunctionComponent = (props) => (
+//  the below is necessary because the FunctionalComponent interface has changed in React>=18
+interface Props {
+  children: React.ReactNode;
+}
+
+const Layout: React.FunctionComponent<Props> = (props) => (
   <div id="main_container" className="font-sans antialiased text-gray-900">
     <main>{props.children}</main>
     <Footer />
