62df2a77c0da8c9aa4a818c95588790fb862ce87
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-05 17:09:55 +0000

    Revert "add i18n to next config"
    This reverts commit 4ffd5454537e810ae9a5d287f5e424d0cf35800f.

--- web_new/next.config.js
+++ web_new/next.config.js
@@ -1,18 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-
-  i18n: {
-    /**
-     * Provide the locales you want to support in your application
-     */
-    locales: ["en-UK", "gd"],
-    /**
-     * This is the default locale you want to be used when visiting
-     * a non-locale prefixed path e.g. `/hello`
-     */
-    defaultLocale: "en-UK",
-  },
 };
 
 module.exports = nextConfig;
