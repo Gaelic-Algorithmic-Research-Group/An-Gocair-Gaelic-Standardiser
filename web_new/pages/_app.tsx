b3cb227fc7037f1ef9398110716aac0f6ed55309
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-06 17:39:03 +0000

    add our custom i18n provider to our custom App

--- web_new/pages/_app.tsx
+++ web_new/pages/_app.tsx
@@ -1,12 +1,24 @@
+// pages/_app.tsx
+// This file overrides Next.js' default `App` component to initialize pages,
+// and allows us to control initialization.
+// More info here: https://nextjs.org/docs/advanced-features/custom-app
 import type { AppProps } from "next/app";
 import Layout from "@/components/Layout";
 import "../styles/globals.css";
 import "../styles/style.css";
+import { useState } from "react";
+import I18nProvider, { I18nLocale } from "@/providers/I18Provider";
 
-export default function MyApp({ Component, pageProps }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+  const [locale, setLocale] = useState<I18nLocale>("en");
+  // wrap the page with our custom i18n provider
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <I18nProvider locale={locale}>
+      {/* add our custom layout */}
+      <Layout>
+        <Component {...pageProps} />{" "}
+        {/* the `Component` prop is the active `page` */}
+      </Layout>
+    </I18nProvider>
   );
 }
