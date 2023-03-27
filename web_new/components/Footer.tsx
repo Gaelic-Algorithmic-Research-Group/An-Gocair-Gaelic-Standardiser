13ee0f81b5185138d86240c8544f336fc98aeeb7
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-27 09:50:57 +0100

    edit footer component

--- web_new/components/Footer.tsx
+++ web_new/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => (
-  <footer>
-    <p>This is an example footer!</p>
+  <footer className="w-full">
+    <nav className="container mx-auto flex items-center justify-between flex-wrap"></nav>
   </footer>
 );
 
