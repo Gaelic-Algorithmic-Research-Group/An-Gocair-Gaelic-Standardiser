4f8ab653b6552b1fddef0cce2146662ab472c61c
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-27 15:33:02 +0100

    rename layout.tsx

--- web_new/components/layout.tsx
+++ web_new/components/layout.tsx
@@ -5,11 +5,11 @@ import SocialShare from "./social-share";
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <>
+    <div id="main_container" className="font-sans antialiased text-gray-900">
       <Header />
       <SocialShare />
       <main>{children}</main>
       <Footer />
-    </>
+    </div>
   );
 }
