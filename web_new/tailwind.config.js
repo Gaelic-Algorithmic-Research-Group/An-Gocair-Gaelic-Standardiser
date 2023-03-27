63d32c317d231ee3846cf2fe626ec61c4e370398
David Salvador Jasin <dsalvadorjasin@turing.ac.uk>
2023-03-27 17:08:23 +0100

    edit theme in tailwind.config.js

--- web_new/tailwind.config.js
+++ web_new/tailwind.config.js
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
+    colors: {
+      transparent: "transparent",
+      white: "#ffffff",
+      gray: colors.slate,
+      red: colors.red,
+      orange: colors.orange,
+      amber: colors.amber,
+      yellow: colors.yellow,
+      lime: colors.lime,
+      green: colors.green,
+      emerald: colors.emerald,
+      teal: colors.teal,
+      cyan: colors.cyan,
+      blue: colors.blue,
+      indigo: colors.indigo,
+      violet: colors.violet,
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
